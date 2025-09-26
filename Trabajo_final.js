998775d2283c640f7645419775a652b016ba6acb
juliana <julusbby530@gmail.com>
2025-09-25 21:10:05 -0500

    final

--- Trabajo_final.js
+++ Trabajo_final.js
@@ -89,3 +89,38 @@ let pedidosPendientes = filtrarPedidosPorEstado("pendiente")
 console.log("Pedidos pendientes:", pedidosPendientes)
 
 
+
+const pedidos = [
+  { estado: "pendiente", total: 30000 },
+  { estado: "preparando", total: 10000 },
+  { estado: "entregado", total: 20000 }
+]
+ 
+function mostrarReportes(pedidos) {
+  let pendientes = 0
+  let preparando = 0
+  let entregados = 0
+  let totalVentas = 0
+ 
+  pedidos.forEach(pedido => {
+    if (pedido.estado == "pendiente") {
+      pendientes++
+    } else if (pedido.estado == "preparando") {
+      preparando++
+    } else if (pedido.estado == "entregado") {
+      entregados++
+      totalVentas += pedido.total;
+    }
+  })
+ 
+  console.log("Pedidos por estado:");
+  console.log("Pendientes:", pendientes)
+  console.log("Preparando:", preparando)
+  console.log("Entregados:", entregados)
+  console.log("Total ventas:", totalVentas)
+}
+ 
+mostrarReportes(pedidos)
+
+
+
